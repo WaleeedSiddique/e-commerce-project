be4cb23ffafbea43c9d6afbf5a66f054a6b360dc
Waleed Siddique <waleed.heaven007@gmail.com>
2023-03-16 13:02:52 +0500

    made dropdown menu for products list

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
-var a = document.getElementById("search-bar")
+var searchInput = document.getElementById("search-bar")
 function displaySearch(){
- a.style.top = "0"
+    searchInput.style.top = "0"
 }
 function closeSearchbar(){
-    a.style.top = "-500px"
+    searchInput.style.top = "-500px"
+}
+let dropmenu = document.getElementById("dropdown") 
+function displayDropdown(){
+    dropmenu.classList.add("show-dropdown")
+}
+ document.getElementById("dropdown").addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+window.onclick = function(event) {
+    console.log("Waleed")
+    if(dropmenu.classList.contains("show-dropdown")){
+        dropmenu.classList.remove("show-dropdown")
+    }
+    // if (!event.target.matches('.product-dropdown')) {
+      
+    //     // document.getElementsByClassName("dropdownmenu-content");
+          
+    //     var i;
+    //     for (i = 0; i < dropdown.length; i++) {
+    //         var openDropdown = dropdown[i];
+    //         if (openDropdown.classList.contains('show')) {
+    //             openDropdown.classList.remove('show');
+    //         }
+    //     }
+    // }
 }
