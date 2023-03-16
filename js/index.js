be632562c5c4b471035b2113c75611a39a10712e
Waleed Siddique <waleed.heaven007@gmail.com>
2023-03-16 14:26:41 +0500

    Dropdown complete but on body click function doesnt work yet

--- js/index.js
+++ js/index.js
@@ -7,26 +7,9 @@ function closeSearchbar(){
 }
 let dropmenu = document.getElementById("dropdown") 
 function displayDropdown(){
-    dropmenu.classList.add("show-dropdown")
+    dropmenu.classList.toggle("show-dropdown")
 }
  document.getElementById("dropdown").addEventListener('click', function (event) {
     event.stopPropagation();
 });
-window.onclick = function(event) {
-    console.log("Waleed")
-    if(dropmenu.classList.contains("show-dropdown")){
-        dropmenu.classList.remove("show-dropdown")
-    }
-    // if (!event.target.matches('.product-dropdown')) {
-      
-    //     // document.getElementsByClassName("dropdownmenu-content");
-          
-    //     var i;
-    //     for (i = 0; i < dropdown.length; i++) {
-    //         var openDropdown = dropdown[i];
-    //         if (openDropdown.classList.contains('show')) {
-    //             openDropdown.classList.remove('show');
-    //         }
-    //     }
-    // }
-}
+
