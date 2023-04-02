cc7905713b05baaa01d90d5c71de43e30fd60201
AneesSiddique <anees@ec.com.pk>
2023-04-02 16:48:22 +0500

    bugs resolved

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 var searchInput = document.getElementById("search-bar")
 var dropmenu = document.getElementById("dropdown") 
 function displaySearch(){
-    searchInput.style.top = "0"
-    console.log("whtever")
+    searchInput.style.top = "0%"
 }
 function closeSearchbar(){
     searchInput.style.top = "-500px"
@@ -10,8 +9,22 @@ function closeSearchbar(){
 
 function displayDropdown(){
     dropmenu.classList.toggle("show-dropdown")
+    console.log("whtever")
 }
  document.getElementById("dropdown").addEventListener('click', function (event) {
     event.stopPropagation();
 });
 
+let nav = document.getElementById("navbar")
+window.onscroll = function() {
+    scroll()
+};
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.style.backgroundColor = "#efede9";
+    }
+    else {
+        nav.style.backgroundColor = "transparent";
+    }
+}
