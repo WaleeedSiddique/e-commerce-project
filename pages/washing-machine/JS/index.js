a4c9c4ee07506d295aae5c660f1750c208539bff
Raheel-Izaz <raheelizaz3@gmail.com>
2023-04-05 13:40:09 +0500

    Issues have been fixed and all pages linked to whole web site

---
+++ pages/washing-machine/JS/index.js
@@ -0,0 +1,30 @@
+var searchInput = document.getElementById("search-bar")
+var dropmenu = document.getElementById("dropdown") 
+function displaySearch(){
+    searchInput.style.top = "0%"
+}
+function closeSearchbar(){
+    searchInput.style.top = "-500px"
+}
+
+function displayDropdown(){
+    dropmenu.classList.toggle("show-dropdown")
+    console.log("whtever")
+}
+ document.getElementById("dropdown").addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+let nav = document.getElementById("navbar")
+window.onscroll = function() {
+    scroll()
+};
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.style.backgroundColor = "#efede9";
+    }
+    else {
+        nav.style.backgroundColor = "transparent";
+    }
+}
